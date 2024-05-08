8a7841f0f1ec169a0bbf0a92924ff2cc246e79ec
Mesh98Dev <158997670+Mesh98Dev@users.noreply.github.com>
2024-05-08 17:06:25 +0300

    tryingg to make realstic move fps
    to be continued

--- Assets/FPSCamera.cs
+++ Assets/FPSCamera.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
-public class PlayerController : MonoBehaviour
+public class FpsCamera : MonoBehaviour
 {
+   
     // Variables for movement speed and rotation speed
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 5f;
+//public float moveSpeed = 5f;
+//public float rotationSpeed = 5f;
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
 
     // Reference to the main camera
-    private Camera mainCamera;
+//private Camera mainCamera;
 
     void Start()
     {
         // Find the main camera in the scene
-        mainCamera = Camera.main;
+//mainCamera = Camera.main;
+//Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f,90f);
+
+        transform.rotation = Quaternion.Euler(xRotation,yRotation,0);
+        orientation.rotation = quaternion.Euler(0,yRotation,0);
+        
         /*
         // Get player input for movement
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -43,16 +63,15 @@ public class PlayerController : MonoBehaviour
 
         // Handle camera rotation (for first-person perspective)
         // Get mouse input for rotation
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+           
 
         // Rotate the player horizontally based on mouse X movement
-        transform.Rotate(Vector3.up * mouseX * rotationSpeed);
+//   transform.Rotate(Vector3.up * mouseX * rotationSpeed);
 
         // Rotate the main camera vertically based on mouse Y movement
         // Note: Clamp the vertical rotation to limit the camera's range of motion
-        Vector3 currentRotation = mainCamera.transform.eulerAngles;
-        float desiredRotationX = currentRotation.x - mouseY;
-        mainCamera.transform.rotation = Quaternion.Euler(desiredRotationX, currentRotation.y, currentRotation.z);
+//Vector3 currentRotation = mainCamera.transform.eulerAngles;
+//float desiredRotationX = currentRotation.x - mouseY;
+//mainCamera.transform.rotation = Quaternion.Euler(desiredRotationX, currentRotation.y, currentRotation.z);
     }
 }
