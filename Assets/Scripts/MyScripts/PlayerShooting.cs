580bbf8a588294823c33a8e024a9d652a7dbb768
Meshal Al-Odayani <Meshal.otaibi@outlook.sa>
2024-05-26 07:39:20 +0300

    adding new scripts plus project folder

--- Assets/Scripts/MyScripts/PlayerShooting.cs
+++ Assets/Scripts/MyScripts/PlayerShooting.cs
@@ -7,6 +7,8 @@ public class PlayerShooting : MonoBehaviour
 
     public Transform firePos;
     public GameObject bullet;
+    public float timeBetweenShoots;
+    public bool canShoots = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canShoots)
         {
             //shoot
             Shoot();
@@ -26,7 +28,13 @@ public class PlayerShooting : MonoBehaviour
         void Shoot() 
         {
             Instantiate(bullet,firePos.position, firePos.rotation);
+             StartCoroutine(ShootDelay());
         }
 
-
+    IEnumerator ShootDelay()
+    {
+        canShoots = false;
+        yield return new WaitForSeconds(timeBetweenShoots);
+        canShoots = true;
+    }
 }
